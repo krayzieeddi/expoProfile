66a69a0e6a4f108f6ca7dd625a2a0824cd12d4ad
Edwin Ho <edwinh300@gmail.com>
2024-04-12 17:24:27 -0700

    expo profile start

--- App.js
+++ App.js
@@ -1,12 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, View, Image } from 'react-native';
+
 
 export default function App() {
   return (
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.titleText}>Edwin's profile page</Text>
+
+      {/* this app only accepts png imgages */}
+      <Image source={require('./assets/logo.png')} style={{width: 50, height: 50}}/>
+
+      <ScrollView style={{height: 50 ,marginHorizontal: 5}}>
+        <Text style={{marginTop: 20}}>
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+        </Text>
+
+        <Text style={{marginTop: 20}}>
+          More random words for filler purposes.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+        </Text>
+
+        <Text style={{marginTop: 20}}>
+          I can't think of anything to say right now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+        </Text>
+
+        <Text style={{marginTop: 20}}>
+          I am typing right now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+        </Text>
+
+        <Text style={{marginTop: 20}}>
+          Really random things for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+          This is some random text to fill up space for now.
+        </Text>
+
+      </ScrollView>
+
+      <View >
+        <TextInput style={{ borderWidth: 1, padding: 10, margin: 10 }} placeholder= "Enter Name here" />
+
+        <TextInput style={{ borderWidth: 1, padding: 10, margin: 10 }} placeholder= "Comment here" />
+
+        <Text style={{ borderWidth: 1, padding: 10, margin: 10, textAlign: 'center',  }}>Submit</Text>
+      </View>
+
     </View>
+    </>
   );
 }
 
@@ -16,5 +77,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'black',
+    height: 300,
+    marginTop: 100,
+    marginHorizontal: 25,
   },
+
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+
 });
